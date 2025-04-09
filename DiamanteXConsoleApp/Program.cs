c53ae641528a59a3f0aabaa46196f47affcaa4c0
Calebe <calebepfinger@gmail.com>
2025-04-09 14:07:23 -0300

    Recebendo valor do usuário e o tratando

--- DiamanteXConsoleApp/Program.cs
+++ DiamanteXConsoleApp/Program.cs
@@ -6,8 +6,16 @@
         {
             Console.WriteLine("Diamante X");
             Console.WriteLine("----------------------");
-            Console.WriteLine("Digite um valor impar:");
+            Console.WriteLine("Digite um valor impar inteiro:");
             Console.WriteLine("----------------------");
+            int tamanho = Convert.ToInt32(Console.ReadLine());
+
+            if (tamanho % 2 == 0)
+            {
+                Console.WriteLine("O valor não pode ser par");
+                Console.WriteLine("Por favor, tente novamente");
+                Console.ReadLine();
+            }
         }
     }
 }
