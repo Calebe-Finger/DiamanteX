7b52b78e07df23005f73ecb5d2a2427943871bba
Calebe <calebepfinger@gmail.com>
2025-04-09 13:49:41 -0300

    Adicionar arquivos de projeto.

---
+++ DiamanteXConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DiamanteXConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------");
+
+            Console.WriteLine("----------------------");
+        }
+    }
+}
