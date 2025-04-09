9de77a63ffcf22560b52d65508c31b5d0b9182b1
Calebe <calebepfinger@gmail.com>
2025-04-09 14:16:49 -0300

    Adicionando mais um tratamento do valor e uma estrutura de repetição para executar o programa "infinitamente"

--- DiamanteXConsoleApp/Program.cs
+++ DiamanteXConsoleApp/Program.cs
@@ -4,17 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Diamante X");
-            Console.WriteLine("----------------------");
-            Console.WriteLine("Digite um valor impar inteiro:");
-            Console.WriteLine("----------------------");
-            int tamanho = Convert.ToInt32(Console.ReadLine());
-
-            if (tamanho % 2 == 0)
+            while (true)
             {
-                Console.WriteLine("O valor não pode ser par");
-                Console.WriteLine("Por favor, tente novamente");
-                Console.ReadLine();
+                Console.WriteLine("Diamante X");
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Digite um valor impar inteiro:");
+                Console.WriteLine("----------------------");
+                int tamanho = Convert.ToInt32(Console.ReadLine());
+                string stgTamaho = tamanho.ToString();
+
+                if (stgTamaho.Contains(".") || stgTamaho.Contains(","))
+                {
+                    Console.WriteLine("O valor deve ser inteiro, não pode conter vírgulas");
+                    Console.WriteLine("Por favor, tente novamente");
+                    Console.ReadLine();
+                    continue;
+
+                }
+
+                if (tamanho % 2 == 0)
+                {
+                    Console.WriteLine("O valor não pode ser par");
+                    Console.WriteLine("Por favor, tente novamente");
+                    Console.ReadLine();
+                }
             }
         }
     }
