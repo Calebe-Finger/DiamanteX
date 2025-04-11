c41151da6815d76d0b3ac902313199c8a8486a35
Calebe <calebepfinger@gmail.com>
2025-04-10 23:18:41 -0300

    Separação do código em Functions

--- DiamanteXConsoleApp/Program.cs
+++ DiamanteXConsoleApp/Program.cs
@@ -8,20 +8,12 @@
             {
                 Console.Clear();        //Limpa o console para a nova operação
 
-                Console.WriteLine("----------------------------------");
-                Console.WriteLine("Diamante X");                                        //Cabeçalho com o nome do programa, 
-                Console.WriteLine("----------------------------------");                // recebimento do valor do losango
-                Console.WriteLine("Digite um valor impar inteiro:");                    // e opção de saída
-                Console.WriteLine("S - Para Sair");
-                Console.WriteLine("----------------------------------");
-                string stgTamanho = Console.ReadLine();
+                string stgTamanho = ExibirCabecalho();      //Exibe o cabeçalho e define o que retornar como 'stgTamanho'
 
-                if (stgTamanho == "S" ||  stgTamanho == "s")            //Verifica se o uúário deseja sair da aplicação
-                {
+                if (stgTamanho == "S" || stgTamanho == "s")            //Verifica se o usuário deseja sair da aplicação
                     break;
-                }
 
-                if (!int.TryParse(Console.ReadLine(), out int tamanho))  //Verifica se valor informado pode ser transformado 
+                if (!int.TryParse(stgTamanho, out int tamanho))  //Verifica se valor informado pode ser transformado 
                 {                                                      //em 'int', para validar que foi informado um número, inteiro.
                     Console.WriteLine("Valor inválido. Por favor, digite um número inteiro.");              
                     Console.WriteLine("Por favor, tente novamente");
@@ -39,39 +31,68 @@
                     continue;
                 }
 
-                Console.WriteLine("----------------------------------");        //Separa a introdução dos resultados
+                Linha();        //Separa a introdução dos resultados
 
-                int x = 1;      //Determina a quantidade de X que serão impressas na tela
-                for (int espaco = (tamanho / 2) + 1; espaco != 0; espaco--)     //Determina a quantidade de espaços em cada linha
-                {
-                    for (int cont = 0; cont < (espaco - 1); cont++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int j = 0; j != x; j++)
-                    {
-                        Console.Write("X");
-                    }
-                    Console.WriteLine("");
-                    x = x + 2;
-                }
+                BaixoDiamante(tamanho);     //Constrói a parde de baixo do diamante
 
-                int y = (tamanho - 2);      //Determina a quantidade de X que serão impressas na tela
-                for (int espaco = 1; espaco != (tamanho / 2) + 1; espaco++)     //Determina a quantidade de espaços em cada linha
-                {
-                    for (int cont = 0; cont != espaco; cont++)
-                    {
-                        Console.Write(" ");
-                    }
-                    for (int j = 0; j != y; j++)
-                    {
-                        Console.Write("X");
-                    }
-                    Console.WriteLine("");
-                    y = y - 2;
-                }
+                CimaDiamante(tamanho);
+
                 Console.ReadLine();
             }
+        }
+
+        static string ExibirCabecalho()
+        {
+            Linha();
+            Console.WriteLine("Diamante X");                             //Cabeçalho com o nome do programa, 
+            Linha();                                                     // recebimento do valor do losango
+            Console.WriteLine("Digite um valor impar inteiro:");         // e opção de saída
+            Console.WriteLine("S - Para Sair");
+            Linha();
+            string opcao = Console.ReadLine();
+
+            return opcao;
+        }
+
+        static void BaixoDiamante(int tamanho)
+        {
+            int x = 1;      //Determina a quantidade de X que serão impressas na tela
+            for (int espaco = (tamanho / 2) + 1; espaco != 0; espaco--)     //Determina a quantidade de espaços em cada linha
+            {
+                for (int cont = 0; cont < (espaco - 1); cont++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j != x; j++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine("");
+                x = x + 2;
+            }
+        }
+
+        static void CimaDiamante(int tamanho)
+        {
+            int y = (tamanho - 2);      //Determina a quantidade de X que serão impressas na tela
+            for (int espaco = 1; espaco != (tamanho / 2) + 1; espaco++)     //Determina a quantidade de espaços em cada linha
+            {
+                for (int cont = 0; cont != espaco; cont++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j != y; j++)
+                {
+                    Console.Write("X");
+                }
+                Console.WriteLine("");
+                y = y - 2;
+            }
+        }
+
+        static void Linha()
+        {
+            Console.WriteLine("----------------------------------");
         }
     }
 }
