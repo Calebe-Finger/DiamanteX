342f6c9eb6effb577171c60d2f6b496b682da0d1
Calebe <calebepfinger@gmail.com>
2025-04-10 22:26:53 -0300

    Comentação do código

--- DiamanteXConsoleApp/Program.cs
+++ DiamanteXConsoleApp/Program.cs
@@ -6,24 +6,24 @@
         {
             while (true)
             {
-                Console.Clear();
+                Console.Clear();        //Limpa o console para a nova operação
 
                 Console.WriteLine("----------------------------------");
-                Console.WriteLine("Diamante X");
-                Console.WriteLine("----------------------------------");
-                Console.WriteLine("Digite um valor impar inteiro:");
+                Console.WriteLine("Diamante X");                                        //Cabeçalho com o nome do programa, 
+                Console.WriteLine("----------------------------------");                // recebimento do valor do losango
+                Console.WriteLine("Digite um valor impar inteiro:");                    // e opção de saída
                 Console.WriteLine("S - Para Sair");
                 Console.WriteLine("----------------------------------");
                 string stgTamanho = Console.ReadLine();
 
-                if (stgTamanho == "S" ||  stgTamanho == "s")
+                if (stgTamanho == "S" ||  stgTamanho == "s")            //Verifica se o uúário deseja sair da aplicação
                 {
                     break;
                 }
 
-                if (!int.TryParse(Console.ReadLine(), out int tamanho))
-                {
-                    Console.WriteLine("Valor inválido. Por favor, digite um número inteiro.");
+                if (!int.TryParse(Console.ReadLine(), out int tamanho))  //Verifica se valor informado pode ser transformado 
+                {                                                      //em 'int', para validar que foi informado um número, inteiro.
+                    Console.WriteLine("Valor inválido. Por favor, digite um número inteiro.");              
                     Console.WriteLine("Por favor, tente novamente");
                     Console.ReadLine();
                     continue;
@@ -33,16 +33,16 @@
 
                 if (tamanho % 2 == 0)
                 {
-                    Console.WriteLine("Valor inválido. Por favor, digite um número ímpar");
+                    Console.WriteLine("Valor inválido. Por favor, digite um número ímpar");         //Valida se o número é par
                     Console.WriteLine("Por favor, tente novamente");
                     Console.ReadLine();
                     continue;
                 }
 
-                Console.WriteLine("----------------------------------");
+                Console.WriteLine("----------------------------------");        //Separa a introdução dos resultados
 
-                int x = 1;
-                for (int espaco = (tamanho / 2) + 1; espaco != 0; espaco--)
+                int x = 1;      //Determina a quantidade de X que serão impressas na tela
+                for (int espaco = (tamanho / 2) + 1; espaco != 0; espaco--)     //Determina a quantidade de espaços em cada linha
                 {
                     for (int cont = 0; cont < (espaco - 1); cont++)
                     {
@@ -56,8 +56,8 @@
                     x = x + 2;
                 }
 
-                int y = (tamanho - 2);
-                for (int espaco = 1; espaco != (tamanho / 2) + 1; espaco++)
+                int y = (tamanho - 2);      //Determina a quantidade de X que serão impressas na tela
+                for (int espaco = 1; espaco != (tamanho / 2) + 1; espaco++)     //Determina a quantidade de espaços em cada linha
                 {
                     for (int cont = 0; cont != espaco; cont++)
                     {
